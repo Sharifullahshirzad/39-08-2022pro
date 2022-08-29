8c863e79ae835429925c3dc95024c1c00bab452b
sharifullah shirzad <sharifullahshirzad81@gmail.com>
2022-08-29 11:45:02 +0430

    file to

---
+++ javascript.js
@@ -0,0 +1,36 @@
+
+
+var x =10;
+if (x=10){
+    document.write('x==10');
+}else{
+    document.write('x ! =10');
+}
+
+//identifiers / names//
+
+
+var FName = 'Sharif'
+var fName = 'Shirzad'
+
+document.write(FName);
+// sharif 
+
+
+let Fname = "asee";
+LET IName = 'nasrati';
+let = sName = "fayaz"
+
+
+var Fname = "fayaz-nasrati";
+
+var first_name_= "asee";
+function myfunc()
+
+
+var x = 10;
+var y = 5;
+var z = x - y;
+ document.write('z')
+ document.getElementById("demo").innerHTML= demo-z Addation_of_x,y;
+
